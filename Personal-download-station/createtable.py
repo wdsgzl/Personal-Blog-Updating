7ed7517e2ef04aee4f5785bf2f8aaf6cd310212d
Arox <812431711@qq.com>
2024-12-31 12:30:38 +0800

    完善了数据库建立的代码，删除了测试用代码

--- Personal-download-station/createtable.py
+++ Personal-download-station/createtable.py
@@ -2,6 +2,7 @@ import sqlite3
 conn = sqlite3.connect('Source.sqlite3')
 
 cur = conn.cursor()
+#建立数据库
 cur.execute("create table Manager(name text,password text)")
 cur.execute("create table Deal(name text,config text,up text)")
 
--- Personal-download-station/execute.py
+++ Personal-download-station/execute.py
@@ -7,13 +7,13 @@ cur = conn.cursor()
 cur.execute("insert into Deal Values('方块人整合包','包含了植物魔法mod','Terrium')")
 
 
-cur.execute("select * from Manager")  #对T_fish表执行数据查找命令
-for Menu_row in cur.fetchall():      #以一条记录为元组单位返回结果给row
-     print(Menu_row)
+cur.execute("select * from Deal")  #对T_fish表执行数据查找命令
+for Deal_row in cur.fetchall():      #以一条记录为元组单位返回结果给row
+     print(Deal_row)
 
-# cur.execute("select * from Manager")  #对T_fish表执行数据查找命令
-# for Manager_row in cur.fetchall():      #以一条记录为元组单位返回结果给row
-#      print(Manager_row)
+cur.execute("select * from Manager")  #对T_fish表执行数据查找命令
+for Manager_row in cur.fetchall():      #以一条记录为元组单位返回结果给row
+     print(Manager_row)
 
 
 conn.commit()
